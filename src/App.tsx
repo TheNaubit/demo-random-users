2b2c2203d7648fa80ed468c41fe1c3698af95067
Alevo <nautadev@gmail.com>
2022-11-16 21:38:18 +0100

    All deps configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+/**
+ * I like to keep this file (App.tsx) outside of the pages folder
+ * since it is more like a container for the pages and not a page
+ * by itself
+ */
+
 import logo from '@assets/logo.svg';
-import './App.css';
+import { useLogger } from '@hooks/useLogger';
+import { withStore } from 'react-context-hook';
+import { Box, Container, Link, Typography } from '@mui/material';
+import { Image } from 'mui-image'
 
 function App() {
+  const { debug } = useLogger({tag: "App"})
+
+  debug("Rendering app container")
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Image src={logo} alt="logo" showLoading />
+        <Typography variant="body1" component="p">
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+        </Typography>
+        <Link
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+      </Box>
+    </Container>
   );
 }
 
-export default App;
+export default withStore(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '@styles/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from '@theme';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
