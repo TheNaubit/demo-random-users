ec00e3ba25dbce5fd61212ed7288879554b3fa00
Alevo <nautadev@gmail.com>
2023-02-20 17:19:46 +0100

    Added livecycle

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,10 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import LivecycleSdk from '@livecycle/sdk'
+
+LivecycleSdk.init()
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
