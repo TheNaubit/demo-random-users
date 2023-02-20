f71f4e0c508bfe72ddcbee7df01b66fcbb0cfa4e
Alevo <nautadev@gmail.com>
2023-02-20 17:30:45 +0100

    Integrated with vercel

--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,6 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import LivecycleSdk from '@livecycle/sdk'
-
-LivecycleSdk.init()
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
